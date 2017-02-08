bd35ce911ef40a48a8e4474a74327c5485717612
Max Boguslavskiy <max.bogus@gmail.com>
2017-02-08 14:09:09 +0300

    * showed tasks on page load; * changed port to 3000; * ordered tasks by priority; * added additional tasks.

--- application.py
+++ application.py
@@ -47,4 +47,4 @@ def tasks_json():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=3000)
--- static/js/app.js
+++ static/js/app.js
@@ -4,32 +4,38 @@
 
 'use strict';
 
-var tasks = angular.module('tasks', []);
+var app = angular.module('tasks', []);
 
-tasks.service('dataService', function ($http) {
-    delete $http.defaults.headers.common['X-Requested-With'];
-    this.getData = function () {
-        // $http() returns a $promise that we can add handlers with .then()
-        return $http({
+app.controller('AngularJSCtrl', function ($scope, $http) {
+    $http(
+        {
             method: 'GET',
-            url: 'http://localhost/tasks/json'
+            url: 'tasks/json'
+        })
+        .then(function (response) {
+            console.log('success');
+            console.log(response.data);
+            $scope.data = response.data.Tasks;
+        }, function (response) {
+            console.log('error');
+            console.log(response.data);
         });
-    }
-});
 
-tasks.controller('AngularJSCtrl', function ($scope, dataService) {
-    $scope.data = null;
-    dataService.getData().then(function (dataResponse) {
-        console.log(dataResponse);
-        $scope.data = dataResponse;
-    });
 });
 
 
 /** TODO:
+ * REQUIRED SCOPE
  В предположении, что сервер запущен на 3000 порту:
- 1. При открытии http://localhost:3000/tasks должен быть виден список задач, чем выше приоритет - тем выше задача.
  3. Новые задачи должны появляться на http://localhost:3000/tasks без перезагрузки страниц.
+ **/
+
+/** TODO:
+ * ADDITIONAL SCOPE
+ В предположении, что сервер запущен на 3000 порту:
+ 1. Причесать визуально страницу
+ 2. Сделать обработку ошибок
+ 3. Добавить лоадинг
  **/
 
 //My request: curl -X POST -d '{"priority":4,"title":"Покрасить забор","description":"Покрасить забор вокруг дома розовым цветом до полуночи сегодня."}' -H "Content-type: application/json" http://localhost:5000/tasks
